7960c31d5903e55dc70ccd1e182f04ff8aa5a7f6
Jane Park <9193jane@gmail.com>
2023-12-23 23:23:09 +0900

    20231223 2-24 function

---
+++ syntax/function.js
@@ -0,0 +1,22 @@
+f123();
+console.log("A");
+console.log("S");
+console.log("D");
+f123();
+console.log("Q");
+console.log("W");
+console.log("E");
+console.log("R");
+f123();
+console.log("Z");
+console.log("X");
+console.log("C");
+console.log("V");
+console.log("B");
+f123();
+
+function f123() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
