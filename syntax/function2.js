399fafffeac97a4d791712a298ea6ebe2796aa09
Jane Park <9193jane@gmail.com>
2023-12-23 23:30:11 +0900

    20231223 2-25 func_Output

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,9 @@ console.log(Math.round(1.6)); // 2
 console.log(Math.round(1.4)); // 1
 
 function sum(first, second) { // 매개채, 매개변수(parameter)
-  console.log(first + second);
+  console.log('a');
+  return first + second;
+  console.log('b');
 }
 
-sum(2, 4);  // 인자(argument)
+console.log(sum(2, 4));  // 인자(argument)
