ff597aa99833bca488e02d9f5387deeb7b35ca42
Jane Park <9193jane@gmail.com>
2023-12-23 21:56:49 +0900

    20231223 2-19 pathname undefined

--- main.js
+++ main.js
@@ -6,11 +6,38 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
 
   if (pathname === "/") {
-    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-      var template = `
+    if (queryData.id === undefined) {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      var title = "WELCOME!";
+      var description = "Hello, Node.js";
+        var template = `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+        </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        var template = `
       <!doctype html>
       <html>
         <head>
@@ -29,14 +56,13 @@ var app = http.createServer(function (request, response) {
         </body>
       </html>
         `;
-      response.writeHead(200);
-      response.end(template);
-    });
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
-    response.end('Not Found');
+    response.end("Not Found");
   }
-
-
 });
 app.listen(3000);
