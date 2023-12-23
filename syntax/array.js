bff6e1d34cf0c4ce50553eea76c450b6fae05bb8
Jane Park <9193jane@gmail.com>
2023-12-23 22:42:53 +0900

    20231223 2-21 Array

---
+++ syntax/array.js
@@ -0,0 +1,14 @@
+// Create
+var arr = ['A', 'B', 'C', 'D'];
+
+// Read
+console.log(arr[0]);
+console.log(arr[2]);
+
+// Update
+arr[2] = 3;
+console.log(arr);
+console.log('length: ' + arr.length);
+
+arr.push('E');
+console.log(arr);
